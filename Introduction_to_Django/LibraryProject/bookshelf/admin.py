212c482c7072b6e8c81da801e99024b107f8a73b
Ernest Awuku Junior <awukuernest@gmail.com>
2025-11-02 20:03:51 +0000

    Add list_filter to BookAdmin for improved admin interface filtering

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -5,3 +5,4 @@ from .models import Book
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
+    list_filter = ('author', 'publication_year') 
