783a20e6e0d44489e61b682512f558a3eeb06f4e
Ernest Awuku Junior <awukuernest@gmail.com>
2025-11-16 20:53:54 +0000

    changes

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,35 +1,45 @@
-from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
-class CustomUserManager(BaseUserManager):
-    """
-    Custom user manager to handle user creation with extra fields.
-    """
 
-    def create_user(self, username, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError("Users must have an email address")
-        email = self.normalize_email(email)
-        user = self.model(username=username, email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, username, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError("Superuser must have is_staff=True.")
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
-        return self.create_user(username, email, password, **extra_fields)
-
-
-class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
-
-    objects = CustomUserManager()
+class Author(models.Model):
+    name = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.username
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
+
+
+class Library(models.Model):
+    name = models.CharField(max_length=255)
+    books = models.ManyToManyField(Book, related_name="libraries")
+
+    class Meta:
+        permissions = [
+            ("can_view_library", "Can view library"),
+            ("can_edit_library", "Can edit library"),
+        ]
+
+    def __str__(self):
+        return self.name
+
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=255)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name="librarian")
+
+    def __str__(self):
+        return self.name
