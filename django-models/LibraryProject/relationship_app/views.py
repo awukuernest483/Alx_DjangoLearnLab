2219c7c16e6a40f510e1fc487db2866d97363b88
Ernest Awuku Junior <awukuernest@gmail.com>
2025-11-16 19:44:39 +0000

    changes

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 
 # ----------------------------
