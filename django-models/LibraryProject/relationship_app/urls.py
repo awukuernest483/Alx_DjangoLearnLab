974d7f93b24183c00e99a9e3641c405932858fb9
Ernest Awuku Junior <awukuernest@gmail.com>
2025-11-16 19:46:41 +0000

    changes

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
-from .views import list_books
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
+from .views import list_books
 
 urlpatterns = [
-    path("books/", views.list_books, name="list_books"),  # Function-based view
-    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),  # Class-based view
+    # Authentication URLs expected by checker
+    path("register/", views.register, name="register"),
+    path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
+
+    # Function-based view
+    path("books/", views.list_books, name="list_books"),
+
+    # Class-based view
+    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
 ]
